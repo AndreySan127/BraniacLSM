0dd4cb378e34f89f6c14449d04ba75d81dbd8dd6
Andrey Sannikov <Sannikov26@gmail.com>
2023-02-19 19:49:23 +0400

    lesson_4 Display model's data

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "markdownify.apps.MarkdownifyConfig",
     "mainapp",
 ]
 
@@ -127,14 +128,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-]
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "markdownify.apps.MarkdownifyConfig",
-    "mainapp",
 ]
